a5757584140274de6ef50722a3863b3c46e873b5
Juri-Jang <jjr84@dgu.ac.kr>
2024-08-27 21:01:58 +0900

    페이징 처리(BE)

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -3,6 +3,9 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -30,13 +33,13 @@ public class BoardController {
     }
 
     @GetMapping("/board/list")
-    public String boardList(Model model){
-        model.addAttribute("list",boardService.boardList()); //실행시 반환된 boardList를 list라는 이름으로 받아서 넘김
+    public String boardList(Model model, @PageableDefault(page=0, size=10, sort="id", direction = Sort.Direction.DESC ) Pageable pageable){
+        model.addAttribute("list",boardService.boardList(pageable)); //실행시 반환된 boardList를 list라는 이름으로 받아서 넘김
 
         return "boardlist";
     }
 
-    @GetMapping("/board/view") //localhost:8080/board/view?id=1
+    @GetMapping("/board/view") //localhost:8080/b oard/view?id=1
     public String boardView(Model model, Integer id){
         model.addAttribute("board",boardService.boardView(id));
         return "boardview";
